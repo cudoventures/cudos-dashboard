e781afc4da401333b24e2befa73fa5e5d44a87a8
Stanislav Filipov <stanfilip0v@gmail.com>
2022-04-11 19:19:22 +0300

    Adding staking page

---
+++ src/containers/Staking/components/Validators/index.tsx
@@ -0,0 +1,35 @@
+import { Box } from '@mui/material'
+
+import useValidators from '../../hooks'
+import Tabs from './components/Tabs'
+
+import TableContainer from './components/Table/styles'
+import ValidatorsTable from './components/Table'
+import ValidatorsCount from './components/Count'
+
+const Validators = () => {
+  const { state, handleTabChange, handleSort, sortItems } = useValidators()
+
+  const items = sortItems(state.items)
+
+  return (
+    <TableContainer>
+      <Box display="flex" justifyContent="space-between">
+        <ValidatorsCount count={state.items.length} />
+        <Tabs handleTabChange={handleTabChange} tab={state.tab} />
+      </Box>
+      <Box
+        sx={{ display: 'flex', overflow: 'hidden', flexDirection: 'column' }}
+      >
+        <ValidatorsTable
+          sortDirection={state.sortDirection}
+          sortKey={state.sortKey}
+          handleSort={handleSort}
+          items={items}
+        />
+      </Box>
+    </TableContainer>
+  )
+}
+
+export default Validators
--- src/containers/Staking/index.tsx
+++ src/containers/Staking/index.tsx
@@ -1,15 +1,21 @@
-import { Box, Typography } from '@mui/material'
+import { Typography } from '@mui/material'
+
+import Validators from './components/Validators'
+import { StakingContainer, ValidatorsContainer } from './styles'
 
 const Staking = () => {
   return (
-    <Box>
+    <StakingContainer>
       <Typography variant="h5" fontWeight={700}>
         Staking
       </Typography>
-      <Typography variant="h6" fontWeight={500} color="text.secondary">
+      <Typography variant="subtitle1" fontWeight={500} color="text.secondary">
         Here are displayed the validator to which you can delegate CUDOS tokens
       </Typography>
-    </Box>
+      <ValidatorsContainer>
+        <Validators />
+      </ValidatorsContainer>
+    </StakingContainer>
   )
 }
 
