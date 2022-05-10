98b4d54945b58e0fefa0863117db830dc8886812
stefanpetkov90 <stefan.petkov90@gmail.com>
2022-05-10 16:44:52 +0300

    removed-config-dependancy

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,5 @@ import tsconfigPaths from 'vite-tsconfig-paths'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [alias(), react(), tsconfigPaths()],
-  define: {
-    global: {}
-  }
+  plugins: [alias(), react(), tsconfigPaths()]
 })
