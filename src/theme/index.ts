5a377fd4f27b3ab6c66f81d11f8ffef04c34d242
stefanpetkov90 <stefan.petkov90@gmail.com>
2022-05-23 17:49:52 +0300

    Staked Tokens Info View

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -35,6 +35,21 @@ const dark = createTheme({
         }
       }
     },
+    MuiPopover: {
+      styleOverrides: {
+        root: {
+          fontSize: '14px'
+        },
+        paper: {
+          background: 'white',
+          boxShadow: '0px 0px 30px rgba(27, 32, 49, 0.7)',
+          borderRadius: '15px',
+          color: 'black',
+          padding: '20px',
+          border: '1px solid white'
+        }
+      }
+    },
     MuiButton: {
       styleOverrides: {
         root: {
