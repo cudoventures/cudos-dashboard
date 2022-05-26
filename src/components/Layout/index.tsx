4efaf311129e6c18fe8c77b15501efac93ca2181
stefanpetkov90 <stefan.petkov90@gmail.com>
2022-05-26 15:08:24 +0300

    revert layout change

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -10,15 +10,15 @@ const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => {
       flexDirection="column"
       sx={{
         paddingLeft: '2rem',
-        height: '100vh',
-        maxWidth: '1440px',
-        margin: 'auto'
+        height: '100vh'
       }}
     >
       <Header />
       <Box display="flex" gap={8} flexGrow={1} sx={{ overflow: 'auto' }}>
         <LeftMenu />
-        <Box>{children}</Box>
+        <Box sx={{ paddingRight: '2rem' }} flexGrow={1}>
+          {children}
+        </Box>
       </Box>
     </Box>
   )
