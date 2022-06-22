8d60893e93ce8e80e1b064625155112f7fa7e4fd
Stanislav Filipov <stanfilip0v@gmail.com>
2022-06-22 13:15:27 +0300

    feat: adding pre-commit checks

--- src/ledgers/ibc-go/codec/upgrade/v1beta1/query.ts
+++ src/ledgers/ibc-go/codec/upgrade/v1beta1/query.ts
@@ -743,8 +743,9 @@ interface Rpc {
   ): Promise<Uint8Array>;
 }
 
-declare var self: any | undefined;
-declare var window: any | undefined;
+declare var self: any | undefined
+declare var window: any | undefined
+declare var global: any | undefined
 var globalThis: any = (() => {
   if (typeof globalThis !== "undefined") return globalThis;
   if (typeof self !== "undefined") return self;
