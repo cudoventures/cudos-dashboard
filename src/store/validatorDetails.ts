5e8ef74c3a2de335452bf61fee572fad74fedd05
stefanpetkov90 <stefan.petkov90@gmail.com>
2022-10-05 16:26:47 +0300

    CUDOS-1689-Add-tooltips

--- src/store/validatorDetails.ts
+++ src/store/validatorDetails.ts
@@ -1,5 +1,14 @@
 import { createSlice } from '@reduxjs/toolkit'
 
+export enum TooltipValidatorMessages {
+  AVAILABLE = `The amount of the current validator's available tokens`,
+  DELEGATE = `The amount of the current validator's personal delegations`,
+  UNBONDING = `The amount of the current validator's personal unbondings`,
+  REWARD = `The amount of the current validator's personal rewards`,
+  COMMISSION = `The amount of the current validator's personal commissions`,
+  VOTING_POWER = `By delegating to a validator, a user delegates voting power. The more voting power a validator has, the more weight they have in the consensus and governance processes.`
+}
+
 export type ValidatorState = {
   validator: string
   operatorAddress: string
