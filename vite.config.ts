9d1b3efe79f839b0d5e905d1b3ab56206c91cf44
SpaghettiOverload <imyours@mail.bg>
2022-11-14 16:13:38 +0200

    CUDOS-1525-update-depedencies-and-fix-warnings

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,16 @@ export default defineConfig({
     svgr({ keepEmittedAssets: true })
   ],
   build: {
-    target: 'esnext'
+    target: 'esnext',
+    chunkSizeWarningLimit: 4000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        }
+      }
+    }
   }
 })
